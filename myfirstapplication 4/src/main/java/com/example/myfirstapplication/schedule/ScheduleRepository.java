06324a2ab99c16ba20b9bf170d5798327d7602f5
dani <dani@email.com>
2022-03-20 13:47:53 +0100

    Organised packages, updated .gitignore

--- myfirstapplication 4/src/main/java/com/example/myfirstapplication/schedule/ScheduleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.myfirstapplication.schedule;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ScheduleRepository extends CrudRepository<Schedule, Integer> {
-}
