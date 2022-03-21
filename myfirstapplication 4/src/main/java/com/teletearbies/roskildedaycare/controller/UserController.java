77d1c49ef903b42dd8b56f4fbe2f1bb1669d851a
Mubíbic <90600712+mubibic@users.noreply.github.com>
2022-03-21 10:02:20 +0100

    beginning on class UserController 2

--- myfirstapplication 4/src/main/java/com/teletearbies/roskildedaycare/controller/UserController.java
+++ myfirstapplication 4/src/main/java/com/teletearbies/roskildedaycare/controller/UserController.java
@@ -1,17 +1,27 @@
 package com.teletearbies.roskildedaycare.controller;
 
+import com.teletearbies.roskildedaycare.entity.User;
 import com.teletearbies.roskildedaycare.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class UserController {
 
-/*    @Autowired
+   @Autowired
     UserService userService;
+/*
+    @RequestMapping(/"userList")
+    public String listAllUsers(Model model) {
+        List<User> listUsers = userService.getAllUsers();
+        model.addAttribute("listUsers", listUsers);
+        return "users";
+    }*/
 
-    @RequestMapping(/userList)*/
 
 
 
