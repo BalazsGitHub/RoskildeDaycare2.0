ba9d6a6b45f21fe34e7d3c9d43941deefd791e85
Mubíbic <90600712+mubibic@users.noreply.github.com>
2022-03-21 09:58:41 +0100

    beginning on class UserController

---
+++ myfirstapplication 4/src/main/java/com/teletearbies/roskildedaycare/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.teletearbies.roskildedaycare.controller;
+
+import com.teletearbies.roskildedaycare.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserController {
+
+/*    @Autowired
+    UserService userService;
+
+    @RequestMapping(/userList)*/
+
+
+
+}
