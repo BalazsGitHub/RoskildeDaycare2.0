987bd1020d81ce9c279d98facc24266e5a88a1d8
Dani <you@example.com>
2022-03-23 16:29:12 +0100

    -Added Class diagram

---
+++ myfirstapplication 4/src/main/resources/daycare.sql
@@ -0,0 +1,8 @@
+#User instructions before first run:
+#1: Run CREATE DATABASE IF NOT EXISTS daycare;
+#2: Uncomment the "public void initialiseDatabase()" method in the "common/InitialData" class
+#3: Run main method in Application
+#4: Comment out the "public void initialiseDatabase()" method in "common/InitialData" class
+#5: Have fun using the application!
+
+CREATE DATABASE IF NOT EXISTS daycare;
